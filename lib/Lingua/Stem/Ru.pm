004e9f891fbb091f12351707777aced9a7059880
Neil Bowers <neil@bowers.com>
2016-02-02 23:35:13 +0000

    Switched to Dist::Zilla

--- lib/Lingua/Stem/Ru.pm
+++ lib/Lingua/Stem/Ru.pm
@@ -1,6 +1,8 @@
 package Lingua::Stem::Ru;
 
+use 5.006;
 use strict;
+use warnings;
 use Exporter;
 use Carp;
 use vars qw (@ISA @EXPORT_OK @EXPORT %EXPORT_TAGS);
@@ -10,8 +12,6 @@ BEGIN {
     @EXPORT_OK   = qw (stem stem_word clear_stem_cache stem_caching);
     %EXPORT_TAGS = ();
 }
-
-our $VERSION = "0.02";
 
 my $Stem_Caching  = 0;
 my $Stem_Cache    = {};
@@ -233,15 +233,13 @@ Aleksandr Guidrevitch <pillgrim@mail.ru>
 
  Lingua::Stem
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2003 by Aldo Calpini <dada@perl.it>
 
 Copyright (C) 2004 by Aleksandr Guidrevitch <pillgrim@mail.ru>
 
-This software may be freely copied and distributed under the same
-terms and conditions as Perl itself, either Perl version 5.8.3
-or, at your option, any later version of Perl 5 you may
-have available..
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
